959ee51188bbfe97ea3fd3b125a42ffee25a01fd
ury <ury@feline.local>
2020-11-03 17:06:31 +0200

    * koohii/read-koohii.rkt: store stories in a hash table

--- koohii/read-koohii.rkt
+++ koohii/read-koohii.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 
+(require racket/path)
+(require racket/list)
+
 (require html-parsing)
 (require sxml/sxpath)
 (require html-writing)
-(require racket/path)
 
 
 
@@ -85,6 +87,26 @@
   (process-file "/Users/ury/work/misc-ch-utils/koohii/koohii/aphasiac2_01.html" #f))
 
 
+(define (same-story s1 s2)
+  (and (string=? (story-hanzi s1) (story-hanzi s2))
+       (equal? (story-sxml-nodes s1) (story-sxml-nodes s2))))
+
+
+(define (is-new-story stry stories-list)
+  (for/and ((old-story stories-list))
+    (not (same-story stry old-story))))
+
+
+(define (add-to-hash curr-story ht)
+  (hash-update! ht (story-hanzi curr-story)
+                (lambda (existing-stories)
+                  (printf "upd: ~a" existing-stories)
+                  (if (is-new-story curr-story existing-stories)
+                      (cons curr-story existing-stories)
+                      existing-stories))
+                (lambda ()  '() ) ))
+
+
 
 (define (get-html-files dirpath)
   (filter (lambda (fp)
@@ -94,8 +116,14 @@
 
 (define (process-dir dirpath)
   (define html-files (get-html-files dirpath))
-  (for ((html-file html-files))
-    (process-file html-file)))
+
+  (set! html-files (take html-files 1 ))
+
+  (let ((ht (make-hash)))
+    (for ((html-file html-files))
+      (process-file html-file
+                    (lambda (story) (add-to-hash story ht))))
+    ht))
 
 
 (define (test-2)
